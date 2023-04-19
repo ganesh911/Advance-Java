56cec68ad1955e00bf3cbc64f5ed5aeb7c6dbc53
Ganesh Jore <95754227+ganesh911@users.noreply.github.com>
2023-04-19 22:02:04 +0530

    Delete Spring_Boot_Crud directory

--- Spring_Boot_Crud/src/main/java/com/spring/boot/Spring_Boot_Crud/repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.boot.Spring_Boot_Crud.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.spring.boot.Spring_Boot_Crud.dto.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Integer>{
-
-}
